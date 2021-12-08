c019abc144d1290ded0ddadcff963cb28d40bbf4
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-12-09 01:45:42 +0200

    write docstring for user model

--- authentication/models.py
+++ authentication/models.py
@@ -16,7 +16,25 @@ ROLE_CHOICES = (
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    
+    """
+        This class represents a basic user.\n
+        Attributes:
+        ----------
+        param email: Describes the email of the user
+        type email: str, unique, max_length=40
+        param password: Describes the password of the user
+        type password: str, max_length=255
+        param first_name: Describes the first name of the user
+        type first_name: str, max_length=30, null=True
+        param last_name: Describes the last name of the user
+        type last_name: str, max_length=30, null=True
+        param patronymic: Describes the patronymic of the user
+        type patronymic: str, max_length=30, null=True
+        param created_at: Describes the date when the user was created. Can't be changed
+        type created_at: int (timestamp)
+        param role: Describes the role of the user
+        type role: int, default=0
+    """
     email = models.EmailField(verbose_name=_('Email'), max_length=40, unique=True, validators=[validate_email])
     password = models.CharField(verbose_name=_('Password'), max_length=255)
     first_name = models.CharField(verbose_name=_('FirstName'), max_length=30, null=True)
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.views import View
 
-# Create your views here.
+from .models import CustomUser
+
+
+class RegisterView(View):
+    template_name = 'authentication/register.html'
+    
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        user = CustomUser.objects.create()
+        user.email = request.POST.get('email')
+        user.set_password(request.POST.get('password'))
+        user.save()
+        login(request, user)
+        return redirect('/')
+    
