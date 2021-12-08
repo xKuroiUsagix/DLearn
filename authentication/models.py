3a6da5916b7c8b5478d8674a95399e66cbde5f78
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-12-09 01:47:42 +0200

    delete unnecessary imports

--- authentication/models.py
+++ authentication/models.py
@@ -1,9 +1,7 @@
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 from django.db import models
-from django.db.utils import DataError
 from django.utils.translation import gettext_lazy as _
 
 from .managers import UserManager
