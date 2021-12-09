f62e0632c9e46f512c8a89ae8d78af8dbca45fa4
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-12-09 16:09:32 +0200

    Course models without tasks(temporary) create

--- course/models.py
+++ course/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from django.db.models.deletion import CASCADE, DO_NOTHING
+from django.utils.translation import gettext_lazy as _
 
+from authentication.models import CustomUser
+
+
+class Course(models.Model):
+    name = models.CharField(verbose_name=_('Name'), max_length=128)
+    owner = models.ForeignKey(CustomUser, on_delete=CASCADE, verbose_name=_('Owner'))
+    group_name = models.CharField(max_length=60, null=True, blank=True, verbose_name=_('GroupName'))
+    join_code = models.CharField(max_length=20, unique=True, verbose_name=_('JoinCode'))
+    created_at = models.DateField(auto_now_add=True, verbose_name=_('CreatedAt'))
+
+
+class UserCourse(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=CASCADE, verbose_name=_('User'))
+    course = models.ForeignKey(Course, on_delete=DO_NOTHING, verbose_name=_('Course'))
+    joined_at = models.DateTimeField(auto_now_add=True, verbose_name=_('JoinedAt'))
