e0aa90482e6a88c41a04b879b5234560542e20fb
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-12-10 17:40:48 +0200

    password validator refactored

--- authentication/validators.py
+++ authentication/validators.py
@@ -10,11 +10,9 @@ def is_password_valid(password):
     Returns:
         bool: returns True if password is valid else False
     """
-    # pattern meaning: password should contains
+    # pattern meaning - password should contains:
     #   at least 1 char
     #   at least 1 number
     #   at least length >= 6
     pattern = r'(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$'
-    if not re.match(pattern, password):
-        return False
-    return True
+    return not bool(re.match(pattern, password))
