81f8740d96be8e83dbe36105f6b4406394c936d3
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-12-10 18:30:04 +0200

    Base tempalte move to homepage app. Other templates refactor

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+    
+    template_name = 'homepage/index.html'
+    
+    def get(self, request):
+        return render(request, self.template_name)
