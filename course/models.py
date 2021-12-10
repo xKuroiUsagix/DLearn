7da671eb1dd5d6c4b8d2fe91b67a1cd958e51311
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-12-10 17:57:14 +0200

    course docstrings to models done

--- course/models.py
+++ course/models.py
@@ -7,7 +7,24 @@ from authentication.models import CustomUser
 
 
 class Course(models.Model):
-    name = models.CharField(verbose_name=_('Name'), max_length=128)
+    """
+        This class represents a course owned by some user.\n
+        Attributes:
+        ----------
+        param name: Describes the name of the course
+        type email: str, max_length=128
+        param owner: Describes the owner of the course
+        type owner: int, CustomUser, on_delete=CASCADE
+        param group_name: Describes the group name of the course
+        type group_name: str, max_length=60, null=True, blank=True
+        param join_code: Describes the code for joining the course (same as login for user account)
+        type join_code: str, max_length=20, unique=True
+        param password: Describes the password for the course to join
+        type password: str, max_length=128
+        param created_at: Describes the date when the course was created
+        type created_at: timestamp, auto_now_add=True
+    """
+    name = models.CharField(max_length=128, verbose_name=_('Name'))
     owner = models.ForeignKey(CustomUser, on_delete=CASCADE, verbose_name=_('Owner'))
     group_name = models.CharField(max_length=60, null=True, blank=True, verbose_name=_('GroupName'))
     join_code = models.CharField(max_length=20, unique=True, verbose_name=_('JoinCode'))
@@ -17,10 +34,22 @@ class Course(models.Model):
     _password = None
     
     def set_password(self, raw_password):
+        """Sets the course password
+
+        Args:
+            raw_password (str): raw password of the course
+        """
         self.password = make_password(raw_password)
         self._password = raw_password
         
     def check_password(self, raw_password):
+        """Cheks the raw_password with course password
+
+        Args:
+            raw_password (str): potential password of the course
+        Return:
+            (bool): True if password and raw_password same, else False
+        """
         def setter(raw_password):
             self.set_password(raw_password)
             self._password = None
@@ -29,6 +58,17 @@ class Course(models.Model):
 
 
 class UserCourse(models.Model):
+    """
+        This class represents an information about users and courses to which they belong to.\n
+        Attributes:
+        ----------
+        param user: Describes the user that belongs to the course
+        type user: int, CustomUser, on_delete=CASCADE
+        param course: Describes the course to which the user belongs to
+        type course: int, Course, on_delete=CASCADE
+        param joined_at: Describes the date and time when the user joined to the course
+        type joined_at: timestamp, auto_now_add=True
+    """
     user = models.ForeignKey(CustomUser, on_delete=CASCADE, verbose_name=_('User'))
     course = models.ForeignKey(Course, on_delete=DO_NOTHING, verbose_name=_('Course'))
     joined_at = models.DateTimeField(auto_now_add=True, verbose_name=_('JoinedAt'))
