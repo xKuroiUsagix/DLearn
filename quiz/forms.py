92bcd05407ce0292766459d4ee8af94ec0eb06eb
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2022-02-17 01:38:02 +0200

    tried some forms. it doen't work

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,11 +1,14 @@
 from django import forms
-from django.forms import fields
 
-from .models import Quiz
+from .models import Quiz, Question
 
 
-class QuizForm(forms.ModelForm):
-    
-    class Meta:
-        model = Quiz
-        fields = '__all__'
+class QuestionForm(forms.Form):
+    quiz = forms.IntegerField()
+    question = forms.CharField(required=True)
+    is_textonly = forms.BooleanField(widget=forms.widgets.CheckboxInput(), required=False)
+
+class OptionForm(forms.Form):
+    question = forms.IntegerField()
+    option = forms.CharField(required=True)
+    is_right = forms.BooleanField(required=False)
--- quiz/views.py
+++ quiz/views.py
@@ -5,6 +5,7 @@ from django.views import View
 
 from task.models import Task
 from .models import Quiz, Question, Option
+from .forms import QuestionForm, OptionForm
 
 
 class QuizCreateView(View):
@@ -21,44 +22,48 @@ class QuizCreateView(View):
             
         task = get_object_or_404(Task, id=task_id)
         context = {
-            'task': task
+            'task': task,
+            'question_form': QuestionForm,
+            'option_form': OptionForm
         }
         return render(request, self.template_name, context)
     
     def post(self, request, course_id, task_id):
         task = get_object_or_404(Task, id=task_id)
-        self.model = self.model()
-        self.model.task = task
-        self.model.save()
+        # self.model = self.model()
+        # self.model.task = task
+        # self.model.save()
         
-        question_start = 'question_'
-        text_answer_start = 'textOnlyFor_'
-        option_start = 'optionForQuestion_'
-        option_value_start = 'optionValueForQuestion_'
-        question_counter = 1
+        for item in request.POST.getlist('question'):
+            print(item)
+        # question_start = 'question_'
+        # text_answer_start = 'textOnlyFor_'
+        # option_start = 'optionForQuestion_'
+        # option_value_start = 'optionValueForQuestion_'
+        # question_counter = 1
         
-        for name in request.POST.keys():
-            if name.startswith(question_start):
-                question_counter += 1
+        # for name in request.POST.keys():
+        #     if name.startswith(question_start):
+        #         question_counter += 1
         
-        for i in range(1, question_counter):
-            question = Question()
-            question.quiz = self.model
-            question.question = request.POST.get(f'{question_start}{i}')
-            question.text_answer = bool(request.POST.get(f'{text_answer_start}{i}'))
-            question.save()
+        # for i in range(1, question_counter):
+        #     question = Question()
+        #     question.quiz = self.model
+        #     question.question = request.POST.get(f'{question_start}{i}')
+        #     question.text_answer = bool(request.POST.get(f'{text_answer_start}{i}'))
+        #     question.save()
             
-            option_counter = 1
-            for name in request.POST.keys():
-                if name.startswith(f'{option_start}{i}'):
-                    option_counter += 1
+        #     option_counter = 1
+        #     for name in request.POST.keys():
+        #         if name.startswith(f'{option_start}{i}'):
+        #             option_counter += 1
             
-            for j in range(1, option_counter):
-                option = Option()
-                option.option = request.POST.get(f'{option_start}{i}_{j}')
-                option.is_right = bool(request.POST.get(f'{option_value_start}{i}_{j}'))
-                option.question = question
-                option.save()
+        #     for j in range(1, option_counter):
+        #         option = Option()
+        #         option.option = request.POST.get(f'{option_start}{i}_{j}')
+        #         option.is_right = bool(request.POST.get(f'{option_value_start}{i}_{j}'))
+        #         option.question = question
+        #         option.save()
         
         return redirect(f'/course/{course_id}/task/{task_id}')
 
