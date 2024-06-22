d7dfc91ba39a68253a34cca9f455a2a135d0093f
kodo <kodokawaiosu@gmail.com>
2024-06-22 18:40:30 +0300

    change word spelling in comment for regexp

--- authentication/validators.py
+++ authentication/validators.py
@@ -11,7 +11,7 @@ def is_password_valid(password):
         bool: returns True if password is valid else False
     """
     # pattern meaning - password should contains:
-    #   at least 1 char
+    #   at least 1 character
     #   at least 1 number
     #   at least length >= 6
     pattern = r'(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$'
